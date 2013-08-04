b5ee06d075fb2b9fb03805233e028451c6ad58f7
Mathew Turner <mathew.turner@y7mail.com>
2013-08-04 15:22:42 -0500

    Created a rails application and added status updates

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+YourEpicLife::Application.config.secret_key_base = '34b728d4d309bd26c33659c2bd3ea801a53f9821e9b2417f24190fbd1af04253a12f6351d927ac13e1eaeaafcba66fc7678d9e65460257f0e98ed8cb119ab4cb'
